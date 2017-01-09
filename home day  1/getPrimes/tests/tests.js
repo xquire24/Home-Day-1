6886655b9a8b6278e2aa4d946b12fb0110c322d1
charles <xquireworld@gmail.com>
2017-01-09 02:50:55 +0000

    fourth commit

--- home day  1/getPrimes/tests/tests.js
+++ home day  1/getPrimes/tests/tests.js
@@ -22,6 +22,13 @@ it("should return object of type array for 10", function() {
 it("should return object of type array for 30", function() {
       expect(getPrimes(30)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
     });
+it("should return object of type array for 50", function() {
+      expect(getPrimes(50)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29,31, 37, 41, 43, 47]);
+    });
+
+it("should return object of type array for 5", function() {
+      expect(getPrimes(1)).toEqual([]);
+    });
 
 
 });
