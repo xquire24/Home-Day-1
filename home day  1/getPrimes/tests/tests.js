37b958351dda3bbd9b8c8dd4e484828f7534b181
charles <xquireworld@gmail.com>
2017-01-09 02:52:30 +0000

    fifth commit

--- home day  1/getPrimes/tests/tests.js
+++ home day  1/getPrimes/tests/tests.js
@@ -30,5 +30,13 @@ it("should return object of type array for 5", function() {
       expect(getPrimes(1)).toEqual([]);
     });
 
+it("should return object of type array for 45", function() {
+      expect(getPrimes(45)).toEqual([2,3,5,7, 11, 13, 17, 19, 23, 29,31, 37, 41, 43]);
+    });
+
+it("should return object of type array for 100", function() {
+      expect(getPrimes(100)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]);
+    });
+
 
 });
